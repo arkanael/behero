928be78c6be9c35f4c8aae23edd3812e5cc1a7ff
Luiz Guilherme Bandeira <arkanael@gmail.com>
2024-04-28 13:41:25 -0300

    Terminado o componente de listagem de casos

--- frontend/src/RoutesIndex.jsx
+++ frontend/src/RoutesIndex.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function RoutesIndex(){
     return(
@@ -9,6 +10,8 @@ export default function RoutesIndex(){
         <Routes>
           <Route path="/" exact Component={Logon} />
           <Route path="/register" Component={Register} />
+          <Route path="/profile" Component={Profile} />
+
         </Routes>
       </BrowserRouter>
     );
