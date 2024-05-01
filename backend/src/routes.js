faab29602bf957969b530ae97354a8197297f1dc
Luiz Guilherme Bandeira <arkanael@gmail.com>
2024-05-01 12:08:59 -0300

    Backend finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const OngsController = require('./controllers/OngControllers');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
 routes.get('/', (request, response) =>{
-    return response.send('Hello World')
+    return response.send('ok')
 });
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/profile', ProfileController.get);
 
 routes.post('/ongs', OngsController.create);
 routes.get('/ongs', OngsController.get);
